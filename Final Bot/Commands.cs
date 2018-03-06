cd374e4b8e8d589ad29f2071b53c301a1ab8b32b
alexcoralt <alexcoralt@1ASIX-PC15->
2018-03-06 12:15:15 +0100

    MOre

--- Final Bot/Commands.cs
+++ Final Bot/Commands.cs
@@ -21,6 +21,7 @@ namespace Final_Bot
             String[] args = null;
 
             if (content.Length > 1) args = content.Skip(1).ToArray();
+            else args = new String[] { null };
 
             switch (command)
             {
@@ -70,12 +71,21 @@ namespace Final_Bot
         private static async Task help(SocketMessage msg)
         {
             delComm(msg);
-            await (await msg.Author.GetOrCreateDMChannelAsync()).SendMessageAsync("Comandos: !help, !pic, !gif, !delete, !addpic, !addgif.");
+            await (await msg.Author.GetOrCreateDMChannelAsync()).SendMessageAsync("Comandos: !help, !pic, !gif, !delete, !addpic, !addgif." +
+                "\n\n     !pic *nombre*  (muestra la foto especificada, puedes ver una lista de fotos con !pic list)" +
+                "\n\n     !gif *nombre*  (muestra el gif especificado, puedes ver una lista de gifs con !gif list)" +
+                "\n\n     !delete  (borra los ultimos 100 mensajes, solo podrás usarlo si eres admin)" +
+                "\n\n     !addpic *nombre* *enlaceDeLaFoto*  (añade la imagen desde el enlace a la lista de imagenes disponibles, una vez hecho puedes usar !pic *nombre* con el nombre que hayas especificado para mostrar la foto)" +
+                "\n\n     !addgif *nombre* *enlaceDelGIF*  (añade el GIF desde el enlace a la lista de GIFs disponibles, una vez hecho puedes usar !gif *nombre* con el nombre que hayas especificado para mostrar el GIF)");
         }
 
         private static async Task pic(SocketMessage msg, string file = null)
         {
-            if (file == null) await msg.Channel.SendMessageAsync("Wich File?");
+            if (file == null)
+            {
+                await msg.Channel.SendMessageAsync("BAAAKAAAAA!!! Como voy a saber que imagen quieres que ponga si no me lo dices?, seguro que querias que pusiera alguna foto mia en la playa... no es que no quiera enseñartelas... p-p-pero " + msg.Author.Mention + " no ecchi!");
+                delComm(msg);
+            }
             else if (file == "list")
             {
                 await (await msg.Author.GetOrCreateDMChannelAsync()).SendMessageAsync("Lista de Imagenes:");
@@ -94,24 +104,54 @@ namespace Final_Bot
                         await (await msg.Author.GetOrCreateDMChannelAsync()).SendMessageAsync(all);
                     }
                 }
+                delComm(msg);
             }
             else
             {
-                string fpath = GetFile(file, "img");
-                using (var client = new System.Net.WebClient())
+                try
                 {
-                    client.DownloadFile(fpath, file + ".jpeg");
+                    string fpath = GetFile(file, "img");
+                    using (var client = new System.Net.WebClient())
+                    {
+                        client.DownloadFile(fpath, file + ".jpeg");
+                    }
                 }
-
-                await msg.Channel.SendFileAsync(".\\" + file + ".jpeg");
-                File.Delete(".\\" + file + ".jpeg");
+                catch (Exception ex)
+                {
+                    await msg.Channel.SendMessageAsync(msg.Author.Mention + " esa imagen no existe, dejame que busque si está con la segunda temporada de NGNL... ahh, pues tampoco existe... ");
+                    delComm(msg);
+                }
+                finally
+                {
+                    await msg.Channel.SendFileAsync(".\\" + file + ".jpeg");
+                    File.Delete(".\\" + file + ".jpeg");
+                    delComm(msg);
+                }
             }
-            delComm(msg);
         }
 
         private static async Task gif(SocketMessage msg, string file = null)
         {
-            if (file == null) await msg.Channel.SendMessageAsync("Wich File?");
+            if (file == null) await msg.Channel.SendMessageAsync("BAAAKAAAAA!!! Como voy a saber que gif quieres que ponga si no me lo dices?, seguro que querias que pusiera algun GIF d-d-de... lolis... " + msg.Author.Mention + " no ecchi!");
+            else if (file == "list")
+            {
+                await (await msg.Author.GetOrCreateDMChannelAsync()).SendMessageAsync("Lista de GIFS:");
+                using (StreamReader sr = new StreamReader(".\\resources\\images.xml", true))
+                {
+                    XmlDocument xmlDoc1 = new XmlDocument();
+                    xmlDoc1.Load(sr);
+                    XmlNodeList itemNodes = xmlDoc1.GetElementsByTagName("gif");
+                    if (itemNodes.Count > 0)
+                    {
+                        String all = "";
+                        foreach (XmlElement node in itemNodes)
+                        {
+                            all += ("\n " + node.Attributes["name"].Value.ToString());
+                        }
+                        await (await msg.Author.GetOrCreateDMChannelAsync()).SendMessageAsync(all);
+                    }
+                }
+            }
             else
             {
                 try
@@ -124,7 +164,7 @@ namespace Final_Bot
                 }
                 catch (Exception ex)
                 {
-
+                    await msg.Channel.SendMessageAsync(msg.Author.Mention + " Tu GIF esta en otro castillo! Okno, no existe, pero queria hacerte reir.... ");
                 }
                 finally
                 {
@@ -143,12 +183,10 @@ namespace Final_Bot
             {
                 var deleteMsg = await msg.Channel.GetMessagesAsync(100).Flatten();
                 await msg.Channel.DeleteMessagesAsync(deleteMsg);
-                string logLine = " -- The user " + user.Username + " has used the delete command on channel " + msg.Channel.Name + ". 100 messages have been deleted.";
             }
             else
             {
-                await msg.Channel.SendMessageAsync(user.Mention + " You can't do that");
-                string logLine = " -- The user " + user.Username + " has used the delete command on channel " + msg.Channel.Name + ". Not executed due to lack of permisions.";
+                await msg.Channel.SendMessageAsync(user.Mention + " tu no puedes hacer eso... No es que yo quiera impedírtelo o algo... b-b-b-baka!");
             }
         }
 
